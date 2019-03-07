4656fd4b3a6708715f7daa96e8b6b37113916f59
Sergey Gorbunov <mongant.s@gmail.com>
2019-03-07 21:48:59 +0200

    added createDataBaseStructure method

--- src/main/java/com/gorbunov/dao/dbcp/CreatorDataBase.java
+++ src/main/java/com/gorbunov/dao/dbcp/CreatorDataBase.java
@@ -1,4 +1,16 @@
 package com.gorbunov.dao.dbcp;
 
+import com.gorbunov.utils.ReaderIniFile;
+
 public class CreatorDataBase {
+
+    DataBaseConnection dataBaseConnection = new DataBaseConnection();
+
+    public void createDataBaseStructure() {
+        dataBaseConnection.sqlExecute(ReaderIniFile.iniReader("Schema", "CREATE_SCHEMA"));
+        dataBaseConnection.sqlExecute(ReaderIniFile.iniReader("Table", "CREATE_CLIENT_TABLE"));
+        dataBaseConnection.sqlExecute(ReaderIniFile.iniReader("Table", "CREATE_PRODUCT_TABLE"));
+        dataBaseConnection.sqlExecute(ReaderIniFile.iniReader("Table", "CREATE_PRODUCT_CONTAINER_TABLE"));
+        dataBaseConnection.sqlExecute(ReaderIniFile.iniReader("Table", "CREATE_ORDER_TABLE"));
+    }
 }
